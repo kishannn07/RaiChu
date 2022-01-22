5351e6a194aaa011308c96a115274382b01f60ed
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 19:39:07 +0530

    Update unpin.py

--- adminss/unpin.py
+++ adminss/unpin.py
@@ -2,7 +2,7 @@ from pyrogram import client, filters
 import asyncio
 import time
 from pyrogram.types import ChatPermissions
-from kingbot import kingbot, vr ,Adminsettings
+from RaiChUB import RaiChUB, vr ,Adminsettings
 __MODULE__ = "unpin"
 __HELP__ = """
 __**This command helps you to instantly unpin a message in the chat**__
@@ -10,23 +10,23 @@ __**This command helps you to instantly unpin a message in the chat**__
 -> `unpin`
 """
 
-@kingbot.on_message(filters.command("unpin",vr.get("HNDLR")) & filters.user(Adminsettings))  
+@RaiChUB.on_message(filters.command("unpin",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def unpin_message(_, message):
     msg_id=message.message_id
     chat_id=message.chat.id
     if message.reply_to_message == None:
-        await kingbot.edit_message_text(chat_id , msg_id , "Shall I unpin your head from wall ?")
+        await RaiChUB.edit_message_text(chat_id , msg_id , "Shall I unpin your head from wall ?")
     else:
         if message.chat.type == "private":
             reply_msg_id=message.reply_to_message.message_id
-            await kingbot.unpin_chat_message(chat_id , reply_msg_id )
-            await kingbot.edit_message_text(chat_id , msg_id , "Done the Job master !")
+            await RaiChUB.unpin_chat_message(chat_id , reply_msg_id )
+            await RaiChUB.edit_message_text(chat_id , msg_id , "Done the Job master !")
         else:
-            zuzu=await kingbot.get_chat_member(chat_id , "me")
+            zuzu=await RaiChUB.get_chat_member(chat_id , "me")
             can_pin=zuzu.can_pin_messages
             if can_pin == None:
-                await kingbot.edit_message_text(chat_id , msg_id , "Can't pin messages bruh 🥱") 
+                await RaiChUB.edit_message_text(chat_id , msg_id , "Can't pin messages bruh 🥱") 
             else:         
                 reply_msg_id=message.reply_to_message.message_id
-                await kingbot.unpin_chat_message(chat_id , reply_msg_id)
-                await kingbot.edit_message_text(chat_id , msg_id , "Done the Job master !")
+                await RaiChUB.unpin_chat_message(chat_id , reply_msg_id)
+                await RaiChUB.edit_message_text(chat_id , msg_id , "Done the Job master !")
