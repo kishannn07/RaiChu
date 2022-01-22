c2c47b8b4ab95e19b725786e0d7abfdf0344b6b4
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 19:22:34 +0530

    Update blockuser.py

--- adminss/blockuser.py
+++ adminss/blockuser.py
@@ -2,7 +2,7 @@ from pyrogram import client, filters
 import asyncio
 import time
 from pyrogram.types import ChatPermissions
-from kingbot import kingbot, vr ,Adminsettings
+from RaiChUB import RaiChUB, vr ,Adminsettings
 __MODULE__ = "block"
 __HELP__ = """
 __**This command helps you to block a user in pm**__
@@ -10,7 +10,7 @@ __**This command helps you to block a user in pm**__
 -> `block`
 """
 
-@kingbot.on_message(filters.command("block",vr.get("HNDLR")) & filters.user(Adminsettings))  
+@RaiChUB.on_message(filters.command("block",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def block_user(client, message):
     if not message.chat.type == "private":
         await message.reply("Can't block the group LOL !")
