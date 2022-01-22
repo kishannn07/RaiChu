ddd00149050b69315db204e948997238230bed0d
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 19:50:34 +0530

    Update aliver.py

--- ownerr/aliver.py
+++ ownerr/aliver.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from kingbot import setbot, Adminsettings, kingbot, START_TIME , vr, vcbot
+from RaiChUB import setbot, Adminsettings, kingbot, START_TIME , vr, vcbot
 from datetime import datetime
 import time
 __MODULE__ = "alive"
@@ -9,8 +9,8 @@ __**This command helps you to check wether userbot is alive**__
 ──「 **Usage** 」──
 -> `alive`
 """
-Alive_pic = "https://telegra.ph/file/664686c15a83ccf26239b.mp4"
-@kingbot.on_message(filters.user(Adminsettings) & filters.command("alive",vr.get("HNDLR")))
+Alive_pic = "https://telegra.ph/file/d22b7e6deac7d0141c8c0.jpg"
+@RaiChUB.on_message(filters.user(Adminsettings) & filters.command("alive",vr.get("HNDLR")))
 async def gooe_search(client, message):
     start_time = time.time()
     uptime = (datetime.now() - START_TIME)
@@ -20,8 +20,8 @@ async def gooe_search(client, message):
     ping_time = round((end_time - start_time) * 1000, 3)
     reply_msg += f"🔸Pɪɴɢ Tɪᴍᴇ: **{ping_time}ms\n**"
     reply_msg += f"🔹Kɪɴɢ Uᴘᴛɪᴍᴇ: **{uptime}\n**"
-    reply_msg += f"🔸Sᴜᴘᴘᴏʀᴛ: **@KingUserBots\n**"
-    reply_msg += f"🔹Rᴇᴘᴏ: **[HERE](https://github.com/ToxicCybers/kinguserbot)\n**"
+    reply_msg += f"🔸Sᴜᴘᴘᴏʀᴛ: **@RaiChuOffiCial\n**"
+    reply_msg += f"🔹Rᴇᴘᴏ: **[HERE](https://github.com/ProXSammY/RaiChu)\n**"
     reply_msg += f"🔸Pʏᴛʜᴏɴ: **3.8\n\n**"
     reply_msg += f"🍹Sᴇʀᴠᴇʀꜱ Fᴜɴᴄᴛɪᴏɴɪɴɢ Nᴏʀᴍᴀʟ🍹"
     await client.send_photo(message.chat.id , Alive_pic , reply_msg)
