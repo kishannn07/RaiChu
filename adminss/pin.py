5a991e799fe80db9b17d7e6531b7334a478ed044
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 20:34:40 +0530

    Update pin.py

--- adminss/pin.py
+++ adminss/pin.py
@@ -15,18 +15,18 @@ async def pin_message(_, message):
     msg_id=message.message_id
     chat_id=message.chat.id
     if message.reply_to_message == None:
-        await kingbot.edit_message_text(chat_id , msg_id , "Shall I pin your head to wall ?")
+        await RaiChUB.edit_message_text(chat_id , msg_id , "Shall I pin your head to wall ?")
     else:
         if message.chat.type == "private":
             reply_msg_id=message.reply_to_message.message_id
-            await kingbot.pin_chat_message(chat_id , reply_msg_id , both_sides=True)
+            await RaiChUB.pin_chat_message(chat_id , reply_msg_id , both_sides=True)
             await message.edit_text("Done the Job master !")
         else:
-            zuzu= await kingbot.get_chat_member(chat_id , "me")
+            zuzu= await RaiChUB.get_chat_member(chat_id , "me")
             can_pin=zuzu.can_pin_messages
             if not can_pin:
-                await kingbot.edit_message_text(chat_id , msg_id , "Not a admin bruh 🥱") 
+                await RaiChUB.edit_message_text(chat_id , msg_id , "Not a admin bruh 🥱") 
             else:         
                 reply_msg_id=message.reply_to_message.message_id
-                await kingbot.pin_chat_message(chat_id , reply_msg_id)
-                await kingbot.edit_message_text(chat_id , msg_id , "Done the Job master !")
+                await RaiChUB.pin_chat_message(chat_id , reply_msg_id)
+                await RaiChUB.edit_message_text(chat_id , msg_id , "Done the Job master !")
