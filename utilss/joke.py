1bd466e108a237d2c7be72de28eb3e8a1b195c7e
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 20:16:39 +0530

    Update joke.py

--- utilss/joke.py
+++ utilss/joke.py
@@ -2,7 +2,7 @@ from pyrogram import client, filters
 import asyncio
 import time
 from pyrogram.types import ChatPermissions
-from kingbot import kingbot, vr ,Adminsettings
+from RaiChUB import RaiChUB, vr ,Adminsettings
 from joke_generator import generate
 
 __MODULE__ = "joke"
@@ -12,6 +12,6 @@ __**This command helps you get a joke in the chat**__
 -> `joke`
 """
 
-@kingbot.on_message(filters.command("joke",vr.get("HNDLR")) & filters.user(Adminsettings))  
+@RaiChUB.on_message(filters.command("joke",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def joke(_, message):
     await message.edit_text(generate())
