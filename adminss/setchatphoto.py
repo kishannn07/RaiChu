52350a97b3180d4026e1e7b16b863ad52a9a75ef
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 20:37:57 +0530

    Update setchatphoto.py

--- adminss/setchatphoto.py
+++ adminss/setchatphoto.py
@@ -14,14 +14,14 @@ __**This command helps you set chat photo **__
 async def set_chat_photo(_, message):
     msg_id=message.message_id
     chat_id=message.chat.id
-    zuzu=await kingbot.get_chat_member(chat_id , "me")
+    zuzu=await RaiChUB.get_chat_member(chat_id , "me")
     can_change_admin=zuzu.can_change_info
     can_change_member=message.chat.permissions.can_change_info
     if not (can_change_admin or can_change_member):
         await message.edit_text("You don't have enough permission")
     if message.reply_to_message:
         if message.reply_to_message.photo:
-            await kingbot.set_chat_photo(chat_id , photo=message.reply_to_message.photo.file_id)
+            await RaiChUB.set_chat_photo(chat_id , photo=message.reply_to_message.photo.file_id)
             return
     else:
         await message.edit_text("Reply to a photo to set it !")
