07e875a3b73b4e9f22f8c78176316a155f9ada17
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 19:35:48 +0530

    Update unban.py

--- adminss/unban.py
+++ adminss/unban.py
@@ -2,7 +2,7 @@ from pyrogram import client, filters
 import asyncio
 import time
 from pyrogram.types import ChatPermissions
-from kingbot import kingbot, vr ,Adminsettings
+from RaiChUB import RaiChUB, vr ,Adminsettings
 __MODULE__ = "unban"
 __HELP__ = """
 __**This command helps you to instantly unban a user in the chat**__
@@ -10,7 +10,7 @@ __**This command helps you to instantly unban a user in the chat**__
 -> `unban`
 """
 
-@kingbot.on_message(filters.group & filters.command("unban",vr.get("HNDLR")) & filters.user(Adminsettings))  
+@RaiChUB.on_message(filters.group & filters.command("unban",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def member_unban(_ , message):
     msg_id=message.message_id
     chat_msg=message.text
@@ -26,17 +26,17 @@ async def member_unban(_ , message):
     chat_id=message.chat.id
     me_m =await client.get_me()
     me_ = await message.chat.get_member(int(me_m.id))
-    user_info=await kingbot.get_users(username)
+    user_info=await RaiChUB.get_users(username)
     if me_.can_restrict_members:      
-        await kingbot.unban_chat_member(chat_id , username)
+        await RaiChUB.unban_chat_member(chat_id , username)
         if(user_info.username):
             usercontact=user_info.username
             reply_string="@"+usercontact+" has been picked up from hell 😈"
-            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await RaiChUB.edit_message_text(chat_id , msg_id , reply_string)
         else:
             usercontact=user_info.first_name
             reply_string=usercontact+" has been picked up from 😈"
-            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await RaiChUB.edit_message_text(chat_id , msg_id , reply_string)
     else:
         reply_string="Noob,you can't unban members 😂 !"
         await kingbot.edit_message_text(chat_id , msg_id , reply_string )
