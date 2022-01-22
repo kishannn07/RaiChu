8a1440ba4bb5081e6e8300ab9f0568ed7ee7e761
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 19:33:26 +0530

    Update setchatphoto.py

--- adminss/setchatphoto.py
+++ adminss/setchatphoto.py
@@ -2,7 +2,7 @@ from pyrogram import client, filters
 import asyncio
 import time
 from pyrogram.types import ChatPermissions
-from kingbot import kingbot, vr ,Adminsettings
+from RaiChUB import RaiChUB, vr ,Adminsettings
 __MODULE__ = "setchatphoto"
 __HELP__ = """
 __**This command helps you set chat photo **__
@@ -10,7 +10,7 @@ __**This command helps you set chat photo **__
 -> `setchatphoto`
 """
 
-@kingbot.on_message(filters.group & filters.command("setchatphoto",vr.get("HNDLR")) & filters.user(Adminsettings))  
+@RaiChUB.on_message(filters.group & filters.command("setchatphoto",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def set_chat_photo(_, message):
     msg_id=message.message_id
     chat_id=message.chat.id
